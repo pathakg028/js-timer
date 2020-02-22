7c4ee53479467438d75eaff5fc5919118bd4f28b
Gaurav Pathak <gauravpathak@192-168-1-8.tpgi.com.au>
2020-02-23 08:41:38 +1100

    start and stop functionalities implemented

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+class Timer {
+    constructor(durationInput, startButton, pauseButton){
+        this.startButton = startButton;
+        this.durationInput = durationInput;
+        this.pauseButton = pauseButton;
+    
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+        
+    }
+    start = () => {
+        this.tick();
+        this.interval = setInterval(this.tick, 1000)
+    }
+    pause = () => {
+        clearInterval(this.interval)
+
+    }
+    onDurationChange = () => {
+        console.log("duration is changed");
+    }
+    tick = () => {
+        console.log("tick");
+    }
+}
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#play');
+const  pauseButton= document.querySelector('#pause');
+const timer = new Timer(durationInput, startButton, pauseButton);
